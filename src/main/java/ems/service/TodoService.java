f3591420035e07789574d47dfad19678b05fbc3a
tvdinh <tvdinh@tma.com.vn>
2017-09-07 16:17:33 +0700

    Change username

--- src/main/java/ems/service/TodoService.java
+++ src/main/java/ems/service/TodoService.java
@@ -15,9 +15,9 @@ public class TodoService {
     private static int todoCount = 3;
 
     static {
-        todos.add( new Todo( 1, "in28minutes", "Learn Spring MVC", new Date(), false ) );
-        todos.add( new Todo( 2, "in28minutes", "Learn Strut", new Date(), false ) );
-        todos.add( new Todo( 3, "in28minutes", "Learn Hibernate", new Date(), false ) );
+        todos.add( new Todo( 1, "tvdinh", "Learn Spring MVC", new Date(), false ) );
+        todos.add( new Todo( 2, "tvdinh", "Learn Strut", new Date(), false ) );
+        todos.add( new Todo( 3, "tvdinh", "Learn Hibernate", new Date(), false ) );
     }
 
     public List<Todo> getAll() {
