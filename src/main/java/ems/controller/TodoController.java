9abc4fed646d571954272a8c719626a3ac8b7929
tvdinh <tvdinh@tma.com.vn>
2017-09-07 14:08:34 +0700

    Adding method to TodoController

--- src/main/java/ems/controller/TodoController.java
+++ src/main/java/ems/controller/TodoController.java
@@ -6,11 +6,14 @@ import ems.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("name")
 public class TodoController {
     @Autowired
     private TodoService mTodoService;
@@ -31,5 +34,12 @@ public class TodoController {
     public String showAddTodoPage( ModelMap model ) {
         model.addAttribute( "todo", new Todo() );
         return "todo";
+    }
+
+    @RequestMapping( value = "/add-todo", method = RequestMethod.POST )
+    public String addTodo( ModelMap model, @PathVariable Todo todo) {
+        mTodoService.addTodo( (String)model.get( "name" ), todo.getDesc(), todo.getTargetDate(), false );
+        model.clear();
+        return "redirect:/list-todos";
     }
 }
